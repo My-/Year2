6454e3d1580a54f636a676071b4532d646039c5b
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2017-11-15 23:41:23 +0000

    created PositionTest
    created Position class test file

---
+++ Java/MineSweeper/game.mineSweeper.core/src/test/mineSweeper/core/PositionTest.java
@@ -0,0 +1,40 @@
+package mineSweeper.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PositionTest {
+    @Test
+    void of_Test() {
+    }
+
+    @Test
+    void getX_Test() {
+    }
+
+    @Test
+    void getY_Test() {
+    }
+
+    @Test
+    void isCardinal_Test() {
+    }
+
+    @Test
+    void compareTo_Test() {
+    }
+
+    @Test
+    void equals_Test() {
+    }
+
+    @Test
+    void hashCode_Test() {
+    }
+
+    @Test
+    void toString_Test() {
+    }
+
+}
