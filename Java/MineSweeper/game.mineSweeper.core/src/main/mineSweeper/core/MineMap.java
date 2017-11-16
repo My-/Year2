4df7ab4e733299457aad9ff81c7557e9a67e429e
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2017-11-16 22:40:25 +0000

    Making MineMap class

--- Java/MineSweeper/game.mineSweeper.core/src/main/mineSweeper/core/MineMap.java
+++ Java/MineSweeper/game.mineSweeper.core/src/main/mineSweeper/core/MineMap.java
@@ -8,14 +8,50 @@ public class MineMap {
 
     public final int MINES;
     private int minesLeft;
-    private Set<MineMapPosition> map;
+    private final Set<MineMapPosition> map;
 
-    public MineMap(final int MINES, String map) {
-        this.MINES = MINES;
-        minesLeft = MINES;
+
+    public MineMap( String map) {
         this.map = createMap(map);
+        this.MINES = countMines(this.map);
+        minesLeft = MINES;
     }
 
+    public int getMinesLeft() {
+        return minesLeft;
+    }
+
+    public MineMap markMine(Position position){
+        minesLeft--;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        // TODO: make it more Java8'ish(if possible)
+        String s = "";
+        StringJoiner sj = new StringJoiner(" ");
+        List<MineMapPosition> list = this.map.stream().sorted().collect(toList());
+        LinkedList<MineMapPosition> sortedList = new LinkedList<>(list);
+
+        int prevX = 0;
+        for(MineMapPosition p : sortedList){
+
+            if( prevX < p.X ){
+                s += sj.toString() + "\n";
+                sj = new StringJoiner(" ");
+                prevX = p.X;
+            }
+
+            if( p.getValue() > 10 ){ sj.add((char)p.getValue() +""); }
+            else{ sj.add(p.getValue()+""); }
+        }
+
+        return s += sj.toString().trim(); //TODO: make better
+    }
+
+    /********************************************************/
+    // Private methods
     /**
      * Creates map form String representation of the map.
      *
@@ -23,46 +59,33 @@ public class MineMap {
      * @return created map
      */
     private static Set<MineMapPosition> createMap(String data) {
+        // TODO: make it more Java8'ish(if possible)
         Set<MineMapPosition> map = new HashSet<>();
-        String lines[] = data.split( "\n");
+        String[] lines = data.split( "\n");
+
         for(int i = 0; i < lines.length; i++){
             String[] square = lines[i].split(" ");
+
             for(int j = 0; j < square.length; j++){
                 char ch = square[j].charAt(0); // get value
+
                 if( '0' <= ch && ch <= '9' ){ // if valid number
-                    int n = square[j].charAt(0) - '0'; // convert char to number
+                    int n = square[j].charAt(0) - '0'; // convert char to number ( '5'(char) to 5(int) )
+                    map.add(new MineMapPosition(i, j, n));
+                }else{
+                    int n = square[j].charAt(0); // store character as it representative ascii number
                     map.add(new MineMapPosition(i, j, n));
                 }
-                else{
-                    int n = square[j].charAt(0); // store char as ascii number
-                    map.add(new MineMapPosition(i, j, n));
-                }
+
             }
         }
         return map;
     }
 
-    @Override
-    public String toString() {
-        String s = "";
-        StringJoiner sj = new StringJoiner(" ");
-        List<MineMapPosition> list = this.map.stream().sorted().collect(toList());
-        LinkedList<MineMapPosition> sortedList = new LinkedList<>(list);
-
-        // TODO: fix it
-        int prevX = 0, prevY = 0;
-        for(MineMapPosition p : list){
-
-            if()
-
-            for(int ch : cha){
-                if( ch > 10 ){ sj.add((char)ch +""); }
-                else{ sj.add(ch +""); }
-            }
-            s += sj.toString() + "\n";
-            sj = new StringJoiner(" ");
-        }
-        return s.trim();
+    private static int countMines(Set<MineMapPosition> map){
+        return (int)map.stream()
+                .filter(MineMapPosition::isMine)
+                .count();
     }
 
 }
