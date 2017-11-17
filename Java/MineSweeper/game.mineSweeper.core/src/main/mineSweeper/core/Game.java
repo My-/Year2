ef8f362a4430558f464ff0138fecdd47b7439b0b
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2017-11-17 20:04:14 +0000

    add open(Position):char

--- Java/MineSweeper/game.mineSweeper.core/src/main/mineSweeper/core/Game.java
+++ Java/MineSweeper/game.mineSweeper.core/src/main/mineSweeper/core/Game.java
@@ -2,6 +2,8 @@ package mineSweeper.core;
 
 import java.util.function.Predicate;
 
+import mineSweeper.Data;
+
 public class Game {
 
     private MineMap userMap;
@@ -13,13 +15,22 @@ public class Game {
 
     public Game(String[] data) {
         mineMap = new MineMap(data[1]);
-        userMap = new MineMap(data[2]);
+        userMap = new GameMineMap(data[2]);
         // TODO: map validation
     }
 
 
-    public char get(Position position){
-        return (char)-1;
+    public char open(Position position) throws Boom{
+        int value = mineMap.getValue(position);
+        if( value == PosValue.MINE ){
+             throw new Boom(String.format("Boom! it's a mine! (%d, %d)",
+                    position.X, position.Y));
+        }
+        return GameMineMap.toChar(value);
+    }
+
+    public static Game create(int picMap){
+        return new Game(Data.MAP[picMap]);
     }
 
 
