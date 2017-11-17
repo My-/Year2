8b44aeb178adca1450bcef68b32e7d41c8a61ed9
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2017-11-17 01:08:43 +0000

    building Game
    building Game logick

--- Java/MineSweeper/game.mineSweeper.core/src/main/mineSweeper/core/Game.java
+++ Java/MineSweeper/game.mineSweeper.core/src/main/mineSweeper/core/Game.java
@@ -1,9 +1,36 @@
 package mineSweeper.core;
 
+import java.util.function.Predicate;
+
 public class Game {
 
     private MineMap userMap;
     private MineMap mineMap;
+    private static int size_X, size_Y;
 
-    
+    private final static Predicate<Position> inLimits_X = p -> 0 <= p.X && p.X < size_X;
+    private final static Predicate<Position> inLimits_Y = p -> 0 <= p.Y && p.Y < size_Y;
+    public final static Predicate<Position> inLimits = inLimits_X.and(inLimits_Y); // two predicates combined
+
+    public Game(String[] data) {
+        mineMap = new MineMap(data[1]);
+        userMap = new MineMap(data[2]);
+        setLimits(data[1]);
+        // TODO: map validation
+    }
+
+
+    public char get(Position position){
+        return (char)this.userMap;
+    }
+
+
+    private void setLimits(String map) {
+        String[] rows= map.split("\n");
+        this.size_X = rows.length;
+        this.size_Y = rows[0].split(" ").length;
+    }
+
+
+
 }
