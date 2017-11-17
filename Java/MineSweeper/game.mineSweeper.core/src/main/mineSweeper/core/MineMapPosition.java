7bc769730575153e3ed8f3931b43fa30fafcd3b9
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2017-11-17 00:07:23 +0000

    minor code improvments

--- Java/MineSweeper/game.mineSweeper.core/src/main/mineSweeper/core/MineMapPosition.java
+++ Java/MineSweeper/game.mineSweeper.core/src/main/mineSweeper/core/MineMapPosition.java
@@ -1,16 +1,20 @@
 package mineSweeper.core;
 
+import java.util.function.Predicate;
+
 public class MineMapPosition extends Position {
 
     public final static int MINE = '*';
-    public final static int POSSIBLY_MINE = '?';
-    public final static int UNCHECKED = '_';
+    public final static int POSSIBLY_MINE = '!';
+    public final static int UNKNOWN = '?';
+
+    public final static Predicate<MineMapPosition> isMine = p -> p.getValue() == MINE;
 
     private int value;
 
     public MineMapPosition(Position pos, int value) {
         super(pos);
-        this.value = value;
+        setValue(value);
     }
     public MineMapPosition(int x, int y, int value) {
         this(new Position(x, y), value);
@@ -20,8 +24,8 @@ public class MineMapPosition extends Position {
         return value;
     }
 
-    public void setValue(int value) throws IllegalArgumentException {
-        if( 0 < value && value < 9 || value == MINE || value == POSSIBLY_MINE || value == UNCHECKED ){
+    private void setValue(int value) throws IllegalArgumentException {
+        if( 0 < value && value < 9 || value == MINE || value == POSSIBLY_MINE || value == UNKNOWN ){
             this.value = value;
         }else{
             throw new IllegalArgumentException("Illegal value: "+ value +"("+ (char)value +")");
@@ -29,12 +33,17 @@ public class MineMapPosition extends Position {
 
     }
 
+    /**
+     * This method checks if given position has a MINE as value
+     *
+     *
+     * @param position
+     * @return
+     */
     public static boolean isMine(Position position){
         if( position instanceof MineMapPosition) {
             return ((MineMapPosition) position).getValue() == MINE;
         }
         return false;
     }
-
-
 }
