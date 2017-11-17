808c745aae1d3f2bef5943e15efd905690f57741
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2017-11-17 20:00:39 +0000

    add comment

--- Java/MineSweeper/game.mineSweeper.core/src/main/mineSweeper/core/PosValue.java
+++ Java/MineSweeper/game.mineSweeper.core/src/main/mineSweeper/core/PosValue.java
@@ -3,6 +3,8 @@ package mineSweeper.core;
 import java.util.function.Predicate;
 
 /**
+ * Object to store value
+ *
  * Position value (PosValue) is a position who has value (inheritance).
  * Even composition should be picked over inheritance. In this particular case
  * it makes mo sense (for me) to use "is a" (inheritance) instead of "has a" (composition).
@@ -15,6 +17,7 @@ public class PosValue extends Position {
     public final static int POSSIBLY_MINE = '!';
     public final static int UNKNOWN = '?';
 
+    @Deprecated(forRemoval = true) // Java 9 annotation.
     public final static Predicate<PosValue> isMine = p -> p.getValue() == MINE;
 
     private int value;
@@ -30,12 +33,19 @@ public class PosValue extends Position {
         return new PosValue(x, y, value);
     }
 
+
     public int getValue(){
         return value;
     }
 
-    private void setValue(int value) throws IllegalArgumentException {
-        if( 0 <= value && value < 9 || value == MINE || value == POSSIBLY_MINE || value == UNKNOWN ){
+    /**
+     * Sets value to a given value.
+     *
+     * @param value to set.
+     * @throws IllegalArgumentException if value (0 > value > 9) or not one of (MINE: 'x', POSSIBLY_MINE: '!', UNKNOWN: '?')
+     */
+    void setValue(int value) throws IllegalArgumentException {
+        if( 0 <= value && value < 10 || value == MINE || value == POSSIBLY_MINE || value == UNKNOWN ){
             this.value = value;
         }else{
             throw new IllegalArgumentException("Illegal value: "+ value +"("+ (char)value +")");
@@ -46,10 +56,13 @@ public class PosValue extends Position {
     /**
      * This method checks if given position has a MINE as value
      *
-     *
-     * @param position
-     * @return
+     * @param position to check
+     * @return true if given position is a mine
+     * @deprecated
+     *      This method shouldn't be here. Reason it exist is to demonstrate possible
+     *      Stream.filter() variants (in different class).
      */
+    @Deprecated(forRemoval = true) // Java 9 annotation.
     public static boolean isMine(Position position){
         if( position instanceof PosValue) {
             return ((PosValue) position).getValue() == MINE;
