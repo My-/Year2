22cbf3f6a7afaf3c03e11ee0f2c38d0388e97925
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2017-11-17 14:53:36 +0000

    renamed
    Renamed MineMapPosition to PosValue

--- Java/MineSweeper/game.mineSweeper.core/src/main/mineSweeper/core/PosValue.java
+++ Java/MineSweeper/game.mineSweeper.core/src/main/mineSweeper/core/PosValue.java
@@ -2,22 +2,32 @@ package mineSweeper.core;
 
 import java.util.function.Predicate;
 
-public class MineMapPosition extends Position {
+/**
+ * Position value (PosValue) is a position who has value (inheritance).
+ * Even composition should be picked over inheritance. In this particular case
+ * it makes mo sense (for me) to use "is a" (inheritance) instead of "has a" (composition).
+ * In this particular case I could get away by not using any of them. But I leave
+ * it as it is just as extra security (checking rightness, testing) feature.
+ */
+public class PosValue extends Position {
 
-    public final static int MINE = '*';
+    public final static int MINE = 'x';
     public final static int POSSIBLY_MINE = '!';
     public final static int UNKNOWN = '?';
 
-    public final static Predicate<MineMapPosition> isMine = p -> p.getValue() == MINE;
+    public final static Predicate<PosValue> isMine = p -> p.getValue() == MINE;
 
     private int value;
 
-    public MineMapPosition(Position pos, int value) {
+    public PosValue(Position pos, int value) {
         super(pos);
         setValue(value);
     }
-    public MineMapPosition(int x, int y, int value) {
+    public PosValue(int x, int y, int value) {
         this(new Position(x, y), value);
+    }
+    public static PosValue of(int x, int y, int value){
+        return new PosValue(x, y, value);
     }
 
     public int getValue(){
@@ -41,8 +51,8 @@ public class MineMapPosition extends Position {
      * @return
      */
     public static boolean isMine(Position position){
-        if( position instanceof MineMapPosition) {
-            return ((MineMapPosition) position).getValue() == MINE;
+        if( position instanceof PosValue) {
+            return ((PosValue) position).getValue() == MINE;
         }
         return false;
     }
