ff48cc37bacc4e786e295f6c60eb825e0b6def90
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2017-11-28 21:03:05 +0000

    add comment

---
+++ C/Lab_7/P4/fileAccess.c
@@ -0,0 +1,14 @@
+/*
+Write a program that uses three functions: 
+Instructions() is a function that takes no arguments and returns no value but simply 
+displays the instructions on what the program does using printf statements.
+Average() is a function that takes 3 integer arguments and returns the average of them 
+as a floating point number 
+Display() is a function that takes one floating point number as an argument and 
+displays the value of it. 
+Your program should start by 
+calling Instructions(), then it should read the integers 
+after asking the user for them. 
+It should then call the function Average() to find the average of the three numbers. 
+Finally, it should display the result by calling the function Display(). 
+*/
